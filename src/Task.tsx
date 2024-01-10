626ad5cf63f385f5af3aff7c5dca365dd96ad3f3
eat_code_sleep <141677292+i0am0arunava@users.noreply.github.com>
2024-01-10 22:33:17 +0530

    Update Task.tsx

--- src/Task.tsx
+++ src/Task.tsx
@@ -9,7 +9,7 @@ class Task extends React.Component<Taskpropss> {
           <div className="rounded-md p-2 md:p-4 mt-3 shadow-md border border-slate-100">
             <h2 className="text-base font-bold my-1">{this.props.title}</h2>
             <p className="text-sm text-slate-500">
-              Due Date:
+              Due Dateeeeee:
             </p>
             <p className="text-sm text-slate-500">
               Description: 
